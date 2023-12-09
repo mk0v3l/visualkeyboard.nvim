426d0b48590f2822e0dc5d1257a751439cf10c95
mk0v3l <mickaelkovel@hotmail.com>
2023-12-09 05:27:25 +0100

    final

--- lua/visualkeyboard.lua
+++ lua/visualkeyboard.lua
@@ -2,91 +2,12 @@ local api, fn = vim.api, vim.fn
 
 local M = {}
 
--- function M.onInsertEnter()
---   local curline = api.nvim_win_get_cursor(0)[1]
---   vim.b.insert_top = curline
---   vim.b.insert_bottom = curline
---   vim.b.whitespace_lastline = curline
--- end
-
--- local function atTipOfUndo()
---   local tree = fn.undotree()
---   return tree.seq_last == tree.seq_cur
--- end
-
--- local function stripWhitespace(top, bottom)
---   -- Only do if the buffer is modifiable
---   -- and modified and we are at the tip of an undo tree
---   if not (vim.bo.modifiable and vim.bo.modified and atTipOfUndo()) then
---     return
---   end
-
---   if not top then return end
-
---   -- All conditions passed, go ahead and strip
---   -- Handle the user deleting lines at the bottom
---   local file_bottom = fn.line('$')
-
---   if top > file_bottom then
---     -- The user deleted all the lines; there is nothing to do
---     return
---   end
-
---   vim.b.bottom = math.min(file_bottom, bottom)
-
---   -- Keep the cursor position and these marks:
---   local original_cursor = fn.getcurpos()
---   local first_changed = fn.getpos("'[")
---   local last_changed = fn.getpos("']")
-
---   vim.cmd("silent exe "..top.." ',' "..vim.b.bottom.. " 's/\\v\\s+$//e'")
-
---   fn.setpos("']", last_changed)
---   fn.setpos("'[", first_changed)
---   fn.setpos('.', original_cursor)
--- end
-
--- function M.onTextChanged()
---   -- Text was modified in non-Insert mode.  Use the '[ and '] marks to find
---   -- what was edited and remove its whitespace.
---   stripWhitespace(fn.line("'["), fn.line("']"))
--- end
-
--- function M.onTextChangedI()
---   -- Handle motion this way (rather than checking if
---   -- b:insert_bottom < curline) to catch the case where the user presses
---   -- Enter, types whitespace, moves up, and presses Enter again.
---   local curline = api.nvim_win_get_cursor(0)[1]
-
---   if vim.b.whitespace_lastline < curline then
---       -- User inserted lines below whitespace_lastline
---       vim.b.insert_bottom = vim.b.insert_bottom + (curline - vim.b.whitespace_lastline)
---   elseif vim.b.whitespace_lastline > curline then
---       -- User inserted lines above whitespace_lastline
---       vim.b.insert_top = math.max(1, vim.b.insert_top - (vim.b.whitespace_lastline - curline))
---   end
-
---   vim.b.whitespace_lastline = curline
--- end
-
--- function M.onInsertLeave()
---   stripWhitespace(vim.b.insert_top, vim.b.insert_bottom)
--- end
-
--- function M.onBufLeave()
---   if api.nvim_get_mode().mode == 'i' then
---     stripWhitespace(vim.b.insert_top, vim.b.insert_bottom)
---   end
--- end
-
 local Popup = require("nui.popup")
 local event = require("nui.utils.autocmd").event
 
 local popup = Popup({
 	enter = false,
 	focusable = true,
-	-- enter = true,
-	-- focusable = false,
 	border = {
 		style = "none",
 	},
@@ -95,12 +16,9 @@ local popup = Popup({
 		col = 100,
 	},
 	size = {
-		-- width = "45%", -- nonum
-		-- height = "21%", -- nonum
 		width = "45%",
 		height = "25%",
 	},
-	-- mess = "0000",
 })
 
 local keyboardmaj = {
@@ -126,48 +44,42 @@ local keyboard = {
 	"  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───────────┘",
 }
 
-local togglemajflag = false
-local toggleflag = false
+local majflag = false
+local showflag = false
 
--- vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, keyboardmaj)
 function M.show()
 	vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, keyboard)
 	popup:mount()
+	showflag = true
+	majflag = false
 end
 
 function M.showmaj()
 	vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, keyboardmaj)
 	popup:mount()
+	showflag = true
+	majflag = true
 end
 
 function M.toggle()
-	if toggleflag == false then
+	if showflag == false then
 		popup:mount()
 		popup:show()
-		-- vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, nummaj)
-		-- popup:mount()
-		toggleflag = true
+		showflag = true
 	else
 		popup:hide()
-		-- vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, num)
-		toggleflag = false
+		showflag = false
 	end
 end
 
--- maj=vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, nummaj)
 function M.togglemaj()
-	if togglemajflag == false then
+	if majflag == false then
 		vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, keyboardmaj)
-		togglemajflag = true
+		majflag = true
 	else
 		vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, keyboard)
-		togglemajflag = false
+		majflag = false
 	end
 end
-
------@deprecated
---function M.setup()
---	-- moved to plugin/
---end
 
 return M
